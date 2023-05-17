706c314541098f1baa7a664bded07a49abe5421a
sabin411 <sherasabin411@gmail.com>
2023-05-18 01:44:44 +0545

    ✨feat:Worked with tuples and enum.

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-console.log("your code goes here!");
+/**
+ * Enum: Enumerated global constant identifiers
+ */
+var Role;
+(function (Role) {
+    Role[Role["ADMIN"] = 0] = "ADMIN";
+    Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
+    Role[Role["AUTHOR"] = 2] = "AUTHOR";
+})(Role || (Role = {}));
+/**
+ * Tuples: Fixed length and fixed type array
+ */
+var person = {
+    name: "Maximilian",
+    age: 30,
+    hobbies: ["Sports", "Cooking"],
+    // Tuple
+    role: [2, Role.READ_ONLY],
+};
+if (person.role[0] === Role.ADMIN) {
+    console.log("is admin");
+}
+else {
+    console.log("is not admin");
+}
--- app.ts
+++ app.ts
@@ -1,1 +1,34 @@
-console.log("your code goes here...");
+/**
+ * Enum: Enumerated global constant identifiers
+ */
+enum Role {
+  ADMIN,
+  READ_ONLY,
+  AUTHOR,
+}
+
+/**
+ * Tuples: Fixed length and fixed type array
+ */
+const person: {
+  name: string;
+  age: number;
+  hobbies: string[];
+  role: [number, Role]; // Tuple
+} = {
+  name: "Maximilian",
+  age: 30,
+  hobbies: ["Sports", "Cooking"],
+  // Tuple
+  role: [2, Role.READ_ONLY],
+};
+
+if (person.role[0] === Role.ADMIN) {
+  console.log("is admin");
+} else {
+  console.log("is not admin");
+}
+
+/**
+ * Any type: Any type of value is most flexible type
+ */
